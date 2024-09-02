1ea8861a8fb05aa9ffad39e819816288a958767c
Elynor Sykes <esykes@cherrycreekschools.org>
2024-09-02 22:16:22 +0000

    working code?

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class InsSortInt {
 
+    public static void main(String[] args) throws FileNotFoundException{
+        int[] arr = new int[100]; 
+        int count = 0;
+
+        Scanner scanner = new Scanner(new File("randInts.txt"));
+        while (scanner.hasNextInt() && count < arr.length) {
+            arr[count] = scanner.nextInt();
+            count++;
+        }
+
+        insertionSort(arr, count);
+
+        if (count > 0) {
+            System.out.println("Smallest value: " + arr[0]);
+            System.out.println("Largest value: " + arr[count - 1]);
+        }
+    }
+
+    public static void insertionSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
