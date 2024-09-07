bd358534bc6c7082c34f2e1db7d53c071f7303dd
Elynor Sykes <esykes@cherrycreekschools.org>
2024-09-07 05:10:08 +0000

    Fixed code: larger array

--- InsSortInt.java
+++ InsSortInt.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InsSortInt {
 
     public static void main(String[] args) throws FileNotFoundException{
-        int[] arr = new int[100]; 
+        int[] arr = new int[100000]; 
         int count = 0;
 
         Scanner scanner = new Scanner(new File("randInts.txt"));
@@ -16,22 +16,24 @@ public class InsSortInt {
 
         insertionSort(arr, count);
 
-        if (count > 0) {
-            System.out.println("Smallest value: " + arr[0]);
-            System.out.println("Largest value: " + arr[count - 1]);
+        System.out.println("Smallest value: " + arr[0]);
+        System.out.println("Largest value: " + arr[count - 1]);
+        System.out.println("Entire thing: ");
+        for(int i=0; i<count; i++){
+            System.out.println(arr[i]);
         }
     }
 
     public static void insertionSort(int[] arr, int n) {
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
-
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
             arr[j + 1] = key;
         }
+
     }
 }
